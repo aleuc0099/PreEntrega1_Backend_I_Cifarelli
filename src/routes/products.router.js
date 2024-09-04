807475c4a2c31c4bbbd7fe642e1c7c8f78c23ee0
aleuc <aleuc0099@gmail.com>
2024-09-04 16:50:22 -0300

    Routes put and delete complete

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -38,6 +38,32 @@ router.post("/api/products", (req, res) => {
   res.status(201).json(newProduct);
 });
 
+router.put("/api/products/:pid", (req, res) => {
+  const productId = parseInt(req.params.id);
+  const product = products.find((p) => p.id === productId);
+  if (product) {
+    const { title } = req.body;
+    const { description } = req.body;
+    const { code } = req.body;
+    const { price } = parseInt(req.body);
+    const { stock } = parseInt(req.body);
+    const { category } = req.body;
+    product.title = title;
+    product.description = description;
+    product.code = code;
+    product.price = price;
+    product.stock = stock;
+    product.category = category;
+    res.json(product);
+  } else {
+    res.status(404).json({ error: "product not found" });
+  }
+});
 
+router.delete("/api/products/:pid", (req, res) => {
+  const productId = parseInt(req.params.id);
+  products = products.filter((p) => p.id !== productId);
+  res.json({ msg: "product deleted successfully" });
+});
 
 module.exports = router;
